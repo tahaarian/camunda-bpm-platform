f7b2bdfb42d1288b2786e653e6931a91f9ac335e
Anton von Weltzien <anton.weltzien@camunda.com>
2021-06-28 17:26:54 +0200

    wip

--- webapps/camunda-bpm-sdk-js/lib/api-client/resources/process-definition.js
+++ webapps/camunda-bpm-sdk-js/lib/api-client/resources/process-definition.js
@@ -469,6 +469,19 @@ var ProcessDefinition = AbstractClientResource.extend(
       });
     },
 
+    /**
+     * todo
+     * @param  {uuid}     id    of the process definition to be requested
+     * @param  {Function} [done]
+     */
+    linkedCallableElements: function(id, done) {
+      const path = this.path +'/' + id + '/call-activity-mappings/' ;
+
+      return this.http.get(path, {
+        done: done
+      });
+    },
+
     restartAsync: function(id, params, done) {
       var url = this.path + '/' + id + '/restart-async';
 
